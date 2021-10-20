3af1e6670a36902b13673cdb6191b8c9131b1a70
semtim <ofmafowo@gmail.com>
2021-10-20 17:11:01 +0300

    20/10/2021

--- fit SLSN/fit_ugriz.py
+++ fit SLSN/fit_ugriz.py
@@ -10,6 +10,7 @@ import math
 import warnings
 from sklearn.exceptions import ConvergenceWarning
 import time
+from scipy import stats
 
 temp = os.path.abspath("cut.csv")
 name = pd.read_csv(temp, sep=",")
@@ -40,52 +41,20 @@ for i in cut:
 
 for i in cut:
     kern_size = len( sn[i].bands )
-    if kern_size==3:
-        const_matrix = np.array([[1,0,0],
-                             [0.5,1,0],
-                             [0.5,0.5,1]])
-
-        bounds = [np.array([[1e-4,0,0],
-                        [-1e3,-1e3,0],
-                        [-1e3,-1e3,-1e3]]),
-           np.array([[1e4,0,0],
-                     [1e3,1e3,0],
-                     [1e3,1e3,1e3]])]
-    if kern_size==4:
-        const_matrix = np.array([[1,0,0,0],
-                             [0.5,1,0,0],
-                             [0.5,0.5,1,0],
-                             [0.5,0.5,0.5,1]])
-
-        bounds = [np.array([[1e-4,0,0,0],
-                        [-1e3,-1e3,0,0],
-                        [-1e3,-1e3,-1e3,0],
-                        [-1e3,-1e3,-1e3,-1e3]]),
-           np.array([[1e+4,0,0,0],
-                        [1e3,1e3,0,0],
-                        [1e3,1e3,1e3,0],
-                        [1e3,1e3,1e3,1e3]])]
-    if kern_size==5:
-        const_matrix = np.array([[1,0,0,0,0],
-                             [0.5,1,0,0,0],
-                             [0.5,0.5,1,0,0],
-                             [0.5,0.5,0.5,1,0],
-                             [0.5,0.5,0.5,0,1]])
-
-        bounds = [np.array([[1e-4,0,0,0,0],
-                        [-1e3,-1e3,0,0,0],
-                        [-1e3,-1e3,-1e3,0,0],
-                        [-1e3,-1e3,-1e3,-1e3,0],
-                        [-1e3,-1e3,-1e3,-1e3,-1e3]]),
-           np.array([[1e+4,0,0,0,0],
-                        [1e3,1e3,0,0,0],
-                        [1e3,1e3,1e3,0,0],
-                        [1e3,1e3,1e3,1e3,0],
-                        [1e3,1e3,1e3,1e3,1e3]])]
-    # t = [RBF(1,(1e-4, 1e+4)) for k in range(kern_size-1)]
-    # t.append(ConstantKernel(constant_value=1.0, constant_value_bounds='fixed'))
-    mk = MultiStateKernel( [RBF(1,(1e-4, 1e+4)) for k in range(kern_size)],
-                          const_matrix, bounds )
+    
+    const_matrix = np.eye(kern_size)
+    bound_min = np.eye(kern_size) * (-1e1)
+    bound_max = np.eye(kern_size) * (1e1)
+    for q in range(kern_size):
+        const_matrix[q, 0:q] = 1/2
+        bound_min[q, 0:q] = -1e1
+        bound_max[q, 0:q] = 1e1
+    bounds = [bound_min, bound_max]
+    
+    size_sc = 5
+    scales = stats.expon.rvs(size=size_sc, loc=1e-5, scale = 10)
+    mk = [MultiStateKernel( [RBF(scales[k],(1e-5,1e3)) for k in range(kern_size)],
+                          const_matrix, bounds ) for k in range(size_sc)]
 ####################################################################
         
     x,y=[],[]
@@ -98,96 +67,64 @@ for i in cut:
     # y = np.delete(y,36)
     # err = np.delete(err,36)
     ############################
-    #добавления нуля для того чтобы кривая не уходила за 0
-    # x,y,err=x.tolist(),y.tolist(),err.tolist()
-    # x.append( [4, 56200] )
-    # y.append( 0.1)
-    # err.append( 0.01 )
-    # x,y,err=np.array(x),np.array(y),np.array(err)
-    ############################
+
     mask = []
     for j in range(kern_size):
         mask.append( (x[:,0] == j) )
     
-
-
+###############################################
+    # y_mean = np.array([ np.mean(y*mask[j]) for j in range(kern_size) ])
+    # for j in range(kern_size):
+    #     y[mask[j]] = y[mask[j]]-y_mean[j]
+###############################################
     col = {"u": 'purple',"g": 'green', "r": 'red', "i": 'brown',"z": 'black'}
 
     # with warnings.catch_warnings():
     #     warnings.filterwarnings('error', category=ConvergenceWarning)
     #     try:
     err1 = err**2 + (y/10)**2
-    gpr = GaussianProcessRegressor(kernel=mk, alpha=err1, 
+    
+    gpr = [ GaussianProcessRegressor(kernel=mk[k], alpha=err1, 
                                     n_restarts_optimizer=res).fit(x, y)
-            
+           for k in range(size_sc) ]
+    
+    lml = [ gpr[k].log_marginal_likelihood_value_ for k in range(size_sc) ]
+    ind = np.argmax(lml)
+    gpr = gpr[ind]
                     
     fig, ax = plt.subplots()#figsize=(18, 12),dpi=400
     plt.title( name['Name'][i] )
             
     b = sn[i].bands
-    if kern_size==3:
-        X = np.linspace(min(x[:,1]), max(x[:,1]),1000).reshape(-1,1)
-        X = np.block([[np.zeros_like(X), X],
-                     [np.ones_like(X), X],
-                     [2*np.ones_like(X), X]])
-        predict = gpr.predict(X)
-                
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[:1000], col.get(b[0]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[1000:2000], col.get(b[1]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[2000:],col.get(b[2]))
-        plt.errorbar(x[mask[0],1], y[mask[0]], np.sqrt(err1[mask[0]]), marker='x', ls='', color=col.get(b[0]),ms=3)
-        plt.errorbar(x[mask[1],1], y[mask[1]], np.sqrt(err1[mask[1]]), marker='x', ls='', color=col.get(b[1]),ms=3)
-        plt.errorbar(x[mask[2],1], y[mask[2]], np.sqrt(err1[mask[2]]), marker='x',color=col.get(b[2]), ls='',ms=3)
-        ax.set_xlabel('mjd')
-        ax.set_ylabel('Flux')
-                
-    if kern_size==4:
-        X = np.linspace(min(x[:,1]), max(x[:,1]),1000).reshape(-1,1)
-        X = np.block([[np.zeros_like(X), X],
-                      [np.ones_like(X), X],
-                      [2*np.ones_like(X), X],
-                      [3*np.ones_like(X), X]])
-        predict = gpr.predict(X)
-                
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[:1000], col.get(b[0]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[1000:2000], col.get(b[1]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[2000:3000],col.get(b[2]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[3000:],col.get(b[3]))
-        plt.errorbar(x[mask[0],1], y[mask[0]], np.sqrt(err1[mask[0]]), marker='x', ls='', color=col.get(b[0]),ms=3)
-        plt.errorbar(x[mask[1],1], y[mask[1]], np.sqrt(err1[mask[1]]), marker='x', ls='', color=col.get(b[1]),ms=3)
-        plt.errorbar(x[mask[2],1], y[mask[2]], np.sqrt(err1[mask[2]]), marker='x',color=col.get(b[2]), ls='',ms=3)
-        plt.errorbar(x[mask[3],1], y[mask[3]], np.sqrt(err1[mask[3]]), marker='x',color=col.get(b[3]), ls='',ms=3)
-        ax.set_xlabel('mjd')
-        ax.set_ylabel('Flux')
-            
-    if kern_size==5:
-        X = np.linspace(min(x[:,1]), max(x[:,1]),1000).reshape(-1,1)
-        X = np.block([[np.zeros_like(X), X],
-                      [np.ones_like(X), X],
-                      [2*np.ones_like(X), X],
-                      [3*np.ones_like(X), X],
-                      [4*np.ones_like(X), X]])
-        predict = gpr.predict(X)
-                
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[:1000], col.get(b[0]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[1000:2000], col.get(b[1]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[2000:3000],col.get(b[2]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[3000:4000],col.get(b[3]))
-        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),predict[4000:],col.get(b[4]))
-        plt.errorbar(x[mask[0],1], y[mask[0]], np.sqrt(err1[mask[0]]), marker='x', ls='', color=col.get(b[0]),ms=3)
-        plt.errorbar(x[mask[1],1], y[mask[1]], np.sqrt(err1[mask[1]]), marker='x', ls='', color=col.get(b[1]),ms=3)
-        plt.errorbar(x[mask[2],1], y[mask[2]], np.sqrt(err1[mask[2]]), marker='x',color=col.get(b[2]), ls='',ms=3)
-        plt.errorbar(x[mask[3],1], y[mask[3]], np.sqrt(err1[mask[3]]), marker='x',color=col.get(b[3]), ls='',ms=3)
-        plt.errorbar(x[mask[4],1], y[mask[4]], np.sqrt(err1[mask[4]]), marker='x',color=col.get(b[4]), ls='',ms=3)
-        ax.set_xlabel('mjd')
-        ax.set_ylabel('Flux')
+    
+    X = np.linspace(min(x[:,1]), max(x[:,1]),1000).reshape(-1,1)
+    X = np.block([ [u*np.ones_like(X), X] for u in range(kern_size) ] )
+
+    predict = gpr.predict(X)
+    
+    # for j in range(kern_size):
+    #     predict[1000*j:1000*j+1000] = predict[1000*j:1000*j+1000] + y_mean[j]
+    #     y[mask[j]] = y[mask[j]]+y_mean[j]
+    
+    for u in range(kern_size):
+        ax.plot(np.linspace(min(x[:,1]), max(x[:,1]), 1000),
+                predict[1000*u:1000*u+1000], col.get(b[u]))
+        
+        plt.errorbar(x[mask[u],1], y[mask[u]], np.sqrt(err1[mask[u]]),
+                     marker='x', ls='', color=col.get(b[u]),ms=3)
+    ax.set_xlabel('mjd')
+    ax.set_ylabel('Flux')
+    
+
             
     t = np.array(b).tolist()
     t = [ 'error '+b[l] for l in range(kern_size) ]
     lab = np.array(b).tolist()+t
     plt.legend(lab,framealpha=0.0,loc='upper right',ncol=2)
-    temp =  str(name['Name'][i])+'_ugriz' + '.png'
-    fig.savefig( fname = temp  , bbox_inches="tight")# , transparent=True
+    # temp =  str(name['Name'][i])+'_ugriz' + '.png'
+    # fig.savefig( fname = temp  , bbox_inches="tight")# , transparent=True
 
+#print(gpr.kernel_.get_params()) # optimized hyperparameters
+#print(gpr.log_marginal_likelihood_value_)
         # except Warning:
         #     continue
